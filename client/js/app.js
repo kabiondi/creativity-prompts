827a10910adc7279070ad8e26876e67493907d5a
kabiondi <kellenbiondi@gmail.com>
2015-01-13 03:28:52 -0800

    added home route

--- client/js/app.js
+++ client/js/app.js
@@ -3,8 +3,8 @@ var app = angular.module('creativityPrompts', ['ngRoute', 'ngResource'])
 	function($routeProvider, $locationProvider) {
 		$routeProvider
 			.when('/', {
-				templateUrl: 'views/edit.html',
-				controller: 'strategiesController'
+				templateUrl: 'views/home.html',
+
 			})
 			.when('/edit', {
 				templateUrl: 'views/edit.html',
